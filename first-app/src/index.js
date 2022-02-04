04c3df5174c8b1068b0d16db298ae4579e9a5d5c
aniruddhha <aniruddha.kudalkar@gmail.com>
2022-02-04 11:12:20 +0530

    react : virtual dom and jsx

--- first-app/src/index.js
+++ first-app/src/index.js
@@ -1,7 +1,13 @@
 import React from 'react'; // what is this?
 import ReactDOM from 'react-dom'; // what is this ?
 
-const ui = <h1> hi ------ </h1>
+const ui = (
+    <div>
+      <input type='text'/> 
+      <input type='button' value='okay'/>
+    </div>
+) // jsx
+
 const container =  document.getElementById('root')
 
 ReactDOM.render(ui, container);
