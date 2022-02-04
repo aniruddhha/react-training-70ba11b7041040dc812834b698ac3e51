58a8f5faaeab8be4ba8fe274cbccb4895c549607
aniruddhha <aniruddha.kudalkar@gmail.com>
2022-02-04 12:54:25 +0530

    react : hook : useState

--- first-app/src/os.js
+++ first-app/src/os.js
@@ -1,16 +1,18 @@
+import { useState } from 'react'
+
 export function Os() {
 
-    let os = 'android'
+    const [os, setOs] = useState('android') // sending request of re-rendering and remebering values during re-render
 
-    const btnClk = () => {
-        os = 'ios'
-        console.log(os)
-    }
+    const onClk = () => setOs('ios')
+
+    const onReset = () => setOs('android')
 
     return (
         <div>
             <h1>Your OS is : {os}</h1>
-            <input type='button' value='Okay' onClick={btnClk}/>
+            <input type='button' value='Okay' onClick={onClk}/>
+            <input type='button' value='Reset' onClick={onReset}/>
         </div>
     )
 }
