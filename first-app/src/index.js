f6c58ef1fae5f38a9e114f7cd978fe5a2cc715f3
aniruddhha <aniruddha.kudalkar@gmail.com>
2022-02-04 10:50:17 +0530

    react : project structure : understanding

--- first-app/src/index.js
+++ first-app/src/index.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'; // what is this?
+import ReactDOM from 'react-dom'; // what is this ?
 
-const ui = <h1> hi </h1>
+const ui = <h1> hi ------ </h1>
 const container =  document.getElementById('root')
 
 ReactDOM.render(ui, container);
