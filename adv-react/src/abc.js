066c6e54280ccac791ea441e6ae04649b9eef4dd
aniruddhha <aniruddha.kudalkar@gmail.com>
2022-02-07 10:10:44 +0530

    hooks : useEffect need

---
+++ adv-react/src/abc.js
@@ -0,0 +1,19 @@
+export function Abc() {
+    // useEffect
+    // handling side effects
+    // what do you mean by side effect ?
+
+    // you to write a code for addition of two numbers
+    return (
+        <>
+            <div>
+                <input type='text' placeholder="Num1" />
+                <input type='text' placeholder="Num2" />
+                <h5>Addition is </h5>  
+            </div>
+            <div>
+                <input type='button' value='Add' />
+            </div>
+        </>
+    )
+}
---
+++ adv-react/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Abc } from './abc'
+
+ReactDOM.render(
+  <>
+   <Abc/>
+  </>,
+  document.getElementById('root')
+);
+
+
