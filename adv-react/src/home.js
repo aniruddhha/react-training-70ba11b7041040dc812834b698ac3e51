5dce1475a567ab1f82b6d1c1ab2e93d13bb66274
aniruddhha <aniruddha.kudalkar@gmail.com>
2022-02-07 14:39:24 +0530

    react : router : navigation

--- adv-react/src/home.js
+++ adv-react/src/home.js
@@ -1,18 +1,29 @@
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+
 export function Home() {
+
+    const navigate = useNavigate()
+
+    const onClk = () => navigate('post')
+
     return(
         <>
+            <div>
+                <input type='button' value='Okay' onClick={onClk}/>
+            </div>
             <ul>
                 <li>
-                    <a href="abc">Abc</a>
+                    <Link to="abc">Abc</Link>
                 </li>
                 <li>
-                    <a href="detailed">Detailed</a>
+                    <Link to="detailed">Detailed</Link>
                 </li>
                 <li>
-                    <a href="win">Window</a>
+                    <Link to="win">Window</Link>
                 </li>
                 <li>
-                    <a href="post">Posts</a>
+                    <Link to="post">Posts</Link>
                 </li>
             </ul>  
         </>
