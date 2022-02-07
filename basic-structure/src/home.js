c8f0564fb6b8564c3022078f94e8316c828bf81e
aniruddhha <aniruddha.kudalkar@gmail.com>
2022-02-07 15:52:58 +0530

    react : css framework : bootstrap integration

---
+++ basic-structure/src/home.js
@@ -0,0 +1,17 @@
+export function Home() {
+
+    return (
+        <>
+            <div className='container'>
+                <div className='row border'>
+                    <div className='col border border-danger'>
+                        <input type='button' className='btn btn-success' value='Okay' />
+                    </div>
+                    <div className='col border border-primary'>
+                        hello
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
---
+++ basic-structure/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Home } from './home'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+ReactDOM.render(
+  <Home/>,
+  document.getElementById('root')
+);
