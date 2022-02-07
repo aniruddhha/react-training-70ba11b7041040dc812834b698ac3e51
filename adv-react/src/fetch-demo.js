f26c1717f596f37dacef94f7447f1f364d7edfd0
aniruddhha <aniruddha.kudalkar@gmail.com>
2022-02-07 12:58:07 +0530

    react : http : making http requests

--- adv-react/src/fetch-demo.js
+++ adv-react/src/fetch-demo.js
@@ -1,4 +1,4 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 export function SocialPost() {
 
@@ -6,16 +6,27 @@ export function SocialPost() {
     // - what do you mean by promise ?
     // - what do you mean by Api ?
 
-    const apiCall = () => fetch('https://jsonplaceholder.typicode.com/posts')
-                        .then( res => console.log(res) )
-                        .catch(err => console.log(err))
+    const [posts, setPosts] = useState([])
+
+    const apiCallGet = () => fetch('https://jsonplaceholder.typicode.com/posts') // get call 
+        .then(res => res.json()) //  extracting json body from complete respone object
+        .catch(err => console.log(err)) // handling error if any
 
     useEffect(() => {
-        apiCall()
-    }, [])
+        apiCallGet().then(json => setPosts(json))
+    }, []) // making api call on initial render only
+
+    const uiPosts = posts.map(post => (
+        <div>
+            <h4>{post.title}</h4>
+            <p>{post.body}</p>
+            <hr />
+        </div>
+    ))
 
     return (
         <>
+            {uiPosts}
         </>
     )
 }
