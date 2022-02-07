1a56964ef04cbaf944b6ea5e69faeb270fc0ba09
aniruddhha <aniruddha.kudalkar@gmail.com>
2022-02-07 14:20:34 +0530

    react : router : basic setup

---
+++ adv-react/src/home.js
@@ -0,0 +1,20 @@
+export function Home() {
+    return(
+        <>
+            <ul>
+                <li>
+                    <a href="abc">Abc</a>
+                </li>
+                <li>
+                    <a href="detailed">Detailed</a>
+                </li>
+                <li>
+                    <a href="win">Window</a>
+                </li>
+                <li>
+                    <a href="post">Posts</a>
+                </li>
+            </ul>  
+        </>
+    )
+}
--- adv-react/src/index.js
+++ adv-react/src/index.js
@@ -4,11 +4,32 @@ import { Abc } from './abc'
 import { WindowComp } from './window'
 import { Detailed } from './detailed'
 import { SocialPost } from './fetch-demo'
+import { Home } from './home'
  
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+
+// React Router - https://reactrouter.com/docs/en/v6/getting-started/overview
+// npm install react-router-dom@6
+
+const router = (
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={ <Home/> }/>  
+      <Route path='abc' element={ <Abc/> }/>
+      <Route path='win' element={ <WindowComp/> }/>
+      <Route path='detailed' element={ <Detailed/> }/>
+      <Route path='post' element={ <SocialPost/> }/>
+    </Routes>
+  </BrowserRouter>
+)
+
+
 ReactDOM.render(
-  <>
-   <SocialPost/>
-  </>,
+  router,
   document.getElementById('root')
 );
 
