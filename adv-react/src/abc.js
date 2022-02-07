94c2d3e351469b9bc0f1195c112cb4292c726bd4
aniruddhha <aniruddha.kudalkar@gmail.com>
2022-02-07 10:53:23 +0530

    react : hooks : useEffect setup

--- adv-react/src/abc.js
+++ adv-react/src/abc.js
@@ -1,10 +1,12 @@
 
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 export function Abc() {
     // useEffect
     // handling side effects
-    // what do you mean by side effect ?
+    // what do you mean by side effect ? Anything which react says, that is not my resposibility but I can provide a way to handle it is called side effect
+    // The result from non react world  can impact on react parts all such situation are called side effects. To handle side effects, react says that I am giving you
+    // speacial hook called useEffect
 
     // you to write a code for addition of two numbers
 
@@ -13,27 +15,34 @@ export function Abc() {
     const [res, setRes] = useState(0)
 
     const onClk = () => setRes(num1 + num2)
-    const onNum1Change = (e) => setNum1(Number.parseInt(e.target.value)) 
-    const onNum2Change = (e) => setNum2(Number.parseInt(e.target.value)) 
+    const onNum1Change = (e) => setNum1(Number.parseInt(e.target.value))
+    const onNum2Change = (e) => setNum2(Number.parseInt(e.target.value))
     // managing state of input boxes in a controlled way
+
+    useEffect(() => {
+        // handle the side effect
+
+        console.log(`Value of res is ${res}`)
+    })
+
 
     return (
         <>
             <div>
-                <input 
-                    type='text' 
-                    placeholder="Num1" 
+                <input
+                    type='text'
+                    placeholder="Num1"
                     value={num1}
                     onChange={onNum1Change}
-                    />
+                />
 
-                <input 
-                    type='text' 
-                    placeholder="Num2" 
+                <input
+                    type='text'
+                    placeholder="Num2"
                     value={num2}
                     onChange={onNum2Change}
-                    />
-                <h5>Addition is {res}</h5>  
+                />
+                <h5>Addition is {res}</h5>
             </div>
             <div>
                 <input type='button' value='Add' onClick={onClk} />
