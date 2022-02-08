c9c69659c996274de6b96dfc0caebc4aca436528
aniruddhha <aniruddha.kudalkar@gmail.com>
2022-02-08 17:22:45 +0530

    react : memory optimization : useMemo, useCallback, React.memo

--- dreact-unit-testing/src/mem-opt.js
+++ dreact-unit-testing/src/mem-opt.js
@@ -1,14 +1,21 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useCallback, useMemo } from 'react'
+import  React from 'react'
+
 export function MemOptComp() {
 
     const [cnt, setCnt] = useState(0)
     const [posts, setPosts] = useState([])
 
-    const apiCall = () => fetch('https://jsonplaceholder.typicode.com/posts').then(res => res.json())
+    const apiCall = useCallback ( // it remembers the function
+        () => fetch('https://jsonplaceholder.typicode.com/posts').then(res => res.json())
+    )
 
-    useEffect(() => {
+    const removeUnwnated = useMemo(() => posts.map( pst => ({ body : pst.body, title : pst.title } ))) // expensive work has to be done here
+
+    useEffect( () => {
         apiCall().then( pst => setPosts(pst) )
-    },[cnt])
+    }, [cnt])
+    
 
     const onCnt = () => setCnt(cnt + 1)
 
@@ -17,14 +24,23 @@ export function MemOptComp() {
             <div>
                 <input type='button' value='Okay' onClick={onCnt}/>
             </div>
+            <div>
+                <MeomoLPosts posts={posts}/>
+            </div>
         </>
     )
 }
 
-function ListPosts() {
-    
-}
+const MeomoLPosts = React.memo(function ListPosts({ posts }) { // memorizing the data 
+    return posts.map( (pst, ind) => <Post key={ind} title={pst.title} body={pst.body}/> )
+})
 
-function Post() {
-
+function Post({ title, body }) {
+    return(
+        <div>
+            <p>{title}</p>
+            <p>{body}</p>
+            <hr/>
+        </div>
+    )
 }
