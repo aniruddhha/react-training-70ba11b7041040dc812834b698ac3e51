3e06e1f7c5a7ee9d601f455cd818c8a46bdc1dc3
aniruddhha <aniruddha.kudalkar@gmail.com>
2022-02-08 12:30:47 +0530

    react : unit testing : details and click event

--- dreact-unit-testing/src/App.js
+++ dreact-unit-testing/src/App.js
@@ -1,9 +1,19 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [ttl, setTtl] = useState('Welcome to react') 
+
+  const onClk = () => setTtl('clicked')
+
   return (
     <>
-      <h1 data-testid='ttl'>Welcome to react</h1>
+      <h1 data-testid='ttl'>{ttl}</h1>
+
+      <div>
+        <input type='button' value='okay' data-testid='btn' onClick={onClk}/>
+      </div>
     </>
   );
 }
--- dreact-unit-testing/src/App.test.js
+++ dreact-unit-testing/src/App.test.js
@@ -1,8 +1,28 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
 
-test('checking h1', () =>{
-    render(<App/>)
-    const el = screen.getByTestId('ttl')
-    expect(el).toHaveTextContent('Welcome to react')
+/**
+ *  unit testing
+ *  for given inputs 
+ *  chcking expected vs actual results    
+ * 
+ */
+
+test('checking h1', () => { // this is test case
+    render(<App />) // please load app component
+    const h1 = screen.getByTestId('ttl') // get perticular element in side app component
+    expect(h1).toHaveTextContent('Welcome to react') // check expected and actual values
 }) 
+
+// tdd - test driven development
+test('check button click', () => {
+    render(<App />)
+
+    const h1 = screen.getByTestId('ttl') 
+    expect(h1).toHaveTextContent('Welcome to react')
+
+    const btn = screen.getByTestId('btn')
+    fireEvent.click(btn) // simulation of button click
+    
+    expect(h1).toHaveTextContent('clicked')
+})
