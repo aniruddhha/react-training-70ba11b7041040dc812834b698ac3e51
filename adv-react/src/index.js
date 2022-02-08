88dfb298ed509388f29d5b3078a39ac4f7f06344
aniruddhha <aniruddha.kudalkar@gmail.com>
2022-02-08 11:28:32 +0530

    react : context api : basic setup

--- adv-react/src/index.js
+++ adv-react/src/index.js
@@ -6,7 +6,7 @@ import { Detailed } from './detailed'
 import { SocialPost } from './fetch-demo'
 import { Home } from './home'
 import { UseReducerDemo } from './use-reducer-demo'
- 
+
 import {
   BrowserRouter,
   Routes,
@@ -16,21 +16,26 @@ import {
 // React Router - https://reactrouter.com/docs/en/v6/getting-started/overview
 // npm install react-router-dom@6
 
+const AllDataContext = React.createContext()
+const commonData = { url : '' }
+
 const router = (
-  <BrowserRouter>
-    <Routes>
-      <Route path='/' element={ <Home/> }/>  
-      <Route path='abc/:num1/:num2' element={ <Abc/> }/>
-      <Route path='win' element={ <WindowComp/> }/>
-      <Route path='detailed' element={ <Detailed/> }>
-        <Route path='personal' element={ <h1> Personal </h1> }/>
-        <Route path='financial' element={ <h1> Financial </h1> }/>
-      </Route>
-      <Route path='post' element={ <SocialPost/> }/>
-      <Route path='reducer' element={ <UseReducerDemo/> }/>
-      <Route path='*' element={ <h1>  Requested Route is ❌ Not Available </h1>}/>
-    </Routes>
-  </BrowserRouter>
+  
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={ <AllDataContext.Provider value={commonData}><Home /> </AllDataContext.Provider> } />
+        <Route path='abc/:num1/:num2' element={ <AllDataContext.Provider value={commonData}><Abc /></AllDataContext.Provider>} />
+        <Route path='win' element={<AllDataContext.Provider value={commonData}><WindowComp /></AllDataContext.Provider>} />
+        <Route path='detailed' element={<AllDataContext.Provider value={commonData}><Detailed /></AllDataContext.Provider>}>
+          <Route path='personal' element={<h1> Personal </h1>} />
+          <Route path='financial' element={<h1> Financial </h1>} />
+        </Route>
+        <Route path='post' element={<AllDataContext.Provider value={commonData}><SocialPost /></AllDataContext.Provider>} />
+        <Route path='reducer' element={<UseReducerDemo />} />
+        <Route path='*' element={<h1>  Requested Route is ❌ Not Available </h1>} />
+      </Routes>
+    </BrowserRouter>
+  
 )
 
 
